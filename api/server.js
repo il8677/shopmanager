9db0253d077d361e527e7377c361b023b5687a5d
il8677 <jiajiaisaac@gmail.com>
2022-08-08 00:41:48 +0200

    Database config files

--- api/server.js
+++ api/server.js
@@ -6,10 +6,12 @@ const mongoose = require('mongoose');
 
 const PORT = 25565;
 
+var config = require("./config")
+
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+mongoose.connect(config.DB_URL, { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
